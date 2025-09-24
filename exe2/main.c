f2eecd53ad5b2593283b29226e6a7f16f95f4d28
thomasrudge <thomasfr@al.insper.edu.br>
2025-09-24 15:55:08 -0300

    ex2

--- exe2/main.c
+++ exe2/main.c
@@ -8,10 +8,23 @@
 #include "hardware/pwm.h"
 
 const int PWM_0_PIN = 4;
+const int PWM_1_PIN = 6;
 
 void led_task(void *p) {
     gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
     uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
+    pwm_set_clkdiv(slice_num, 125);
+    pwm_set_wrap(slice_num, 100);
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 20);
+    pwm_set_enabled(slice_num, true);
+
+    while (true) {
+    }
+}
+
+void led_task2(void *p) {
+    gpio_set_function(PWM_1_PIN, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(PWM_1_PIN);
     pwm_set_clkdiv(slice_num, 125);
     pwm_set_wrap(slice_num, 100);
     pwm_set_chan_level(slice_num, PWM_CHAN_A, 80);
@@ -26,6 +39,7 @@ int main() {
     printf("Start RTOS \n");
 
     xTaskCreate(led_task, "LED_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led_task2, "LED_Task 2", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
